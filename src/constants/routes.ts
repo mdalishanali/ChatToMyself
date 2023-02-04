6ba234fa95901b008b6de0934285655a1714d2f8
MD ALISHAN ALI <md.alishanali88@gmail.com>
2023-02-04 14:11:47 +0530

    folder structure created

---
+++ src/constants/routes.ts
@@ -0,0 +1,17 @@
+export default {
+        LOGIN: 'Login',
+        REGISTER: 'Register',
+        FORGOT_PASSWORD: 'Forgot Password',
+
+        HOME: 'Home',
+        HOME_TAB: 'Home Tab',
+        HOME_DRAWER: 'Home Drawer',
+
+        NOTIFICATION: 'Notification',
+        GALLERY: 'Gallery',
+        GALLERY_STACK: 'GalleryStack',
+        NOTE: 'Note',
+        CALCULATOR: 'Calculator',
+        IMAGE_DETAILS:'ImageDetails',
+        IMAGE_VIEW_EDIT:'ImageViewEdit'
+};

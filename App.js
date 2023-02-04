f1018ba0ecff7d27a33e2c34a88ec916a06f0aa4
MD ALISHAN ALI <md.alishanali88@gmail.com>
2023-02-04 15:00:37 +0530

    created a basic two screens

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import OnboardingScreen from "./src/screens/OnboardingScreen";
+import LoginScreen from "./src/screens/LoginScreen";
 
 export default function App() {
+  const AppStack = createNativeStackNavigator();
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <AppStack.Navigator>
+        <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+        <AppStack.Screen name="Login" component={LoginScreen} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
 }

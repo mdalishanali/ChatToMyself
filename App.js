1683294098485066777f8e01f5cf15bd13fa81b0
MD ALISHAN ALI <md.alishanali88@gmail.com>
2023-02-05 10:23:13 +0530

    created auth and app stack

--- App.js
+++ App.js
@@ -4,16 +4,18 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import OnboardingScreen from "./src/screens/OnboardingScreen";
 import LoginScreen from "./src/screens/auth/LoginScreen";
+import Providers from "./src/navigations/index";
 
 export default function App() {
   const AppStack = createNativeStackNavigator();
 
   return (
-    <NavigationContainer>
-      <AppStack.Navigator screenOptions={{ headerShown: false }}>
-        <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
-        <AppStack.Screen name="Login" component={LoginScreen} />
-      </AppStack.Navigator>
-    </NavigationContainer>
+    <Providers />
+    // <NavigationContainer>
+    //   <AppStack.Navigator screenOptions={{ headerShown: false }}>
+    //     <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+    //     <AppStack.Screen name="Login" component={LoginScreen} />
+    //   </AppStack.Navigator>
+    // </NavigationContainer>
   );
 }

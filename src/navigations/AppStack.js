32591f82de7b7b187bdc80f86c6be43c419d43fa
MD ALISHAN ALI <md.alishanali88@gmail.com>
2023-02-25 17:52:48 +0530

    routing fixed

--- src/navigations/AppStack.js
+++ src/navigations/AppStack.js
@@ -8,126 +8,30 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import GroupScreen from "../screens/GroupScreen";
 import ChatScreen from "../screens/ChatScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import QuotesScreen from "../screens/QuotesScreen";
 
-// import HomeScreen from '../screens/HomeScreen';
-// import ChatScreen from '../screens/ChatScreen';
-// import ProfileScreen from '../screens/ProfileScreen';
-// import AddPostScreen from '../screens/AddPostScreen';
-// import MessagesScreen from '../screens/MessagesScreen';
-// import EditProfileScreen from '../screens/EditProfileScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-// const FeedStack = ({navigation}) => (
-//   <Stack.Navigator>
-//     <Stack.Screen
-//       name="RN Social"
-//       component={HomeScreen}
-//       options={{
-//         headerTitleAlign: 'center',
-//         headerTitleStyle: {
-//           color: '#2e64e5',
-//           fontFamily: 'Kufam-SemiBoldItalic',
-//           fontSize: 18,
-//         },
-//         headerStyle: {
-//           shadowColor: '#fff',
-//           elevation: 0,
-//         },
-//         headerRight: () => (
-//           <View style={{marginRight: 10}}>
-//             <FontAwesome5.Button
-//               name="plus"
-//               size={22}
-//               backgroundColor="#fff"
-//               color="#2e64e5"
-//               onPress={() => navigation.navigate('AddPost')}
-//             />
-//           </View>
-//         ),
-//       }}
-//     />
-//     <Stack.Screen
-//       name="AddPost"
-//       component={AddPostScreen}
-//       options={{
-//         title: '',
-//         headerTitleAlign: 'center',
-//         headerStyle: {
-//           backgroundColor: '#2e64e515',
-//           shadowColor: '#2e64e515',
-//           elevation: 0,
-//         },
-//         headerBackTitleVisible: false,
-//         headerBackImage: () => (
-//           <View style={{marginLeft: 15}}>
-//             <Ionicons name="arrow-back" size={25} color="#2e64e5" />
-//           </View>
-//         ),
-//       }}
-//     />
-//     <Stack.Screen
-//       name="HomeProfile"
-//       component={ProfileScreen}
-//       options={{
-//         title: '',
-//         headerTitleAlign: 'center',
-//         headerStyle: {
-//           backgroundColor: '#fff',
-//           shadowColor: '#fff',
-//           elevation: 0,
-//         },
-//         headerBackTitleVisible: false,
-//         headerBackImage: () => (
-//           <View style={{marginLeft: 15}}>
-//             <Ionicons name="arrow-back" size={25} color="#2e64e5" />
-//           </View>
-//         ),
-//       }}
-//     />
-//   </Stack.Navigator>
-// );
-
-// const MessageStack = ({navigation}) => (
-//   <Stack.Navigator>
-//     <Stack.Screen name="Messages" component={MessagesScreen} />
-//     <Stack.Screen
-//       name="Chat"
-//       component={ChatScreen}
-//       options={({route}) => ({
-//         title: route.params.userName,
-//         headerBackTitleVisible: false,
-//       })}
-//     />
-//   </Stack.Navigator>
-// );
-
-// const ProfileStack = ({navigation}) => (
-//   <Stack.Navigator>
-//     <Stack.Screen
-//       name="Profile"
-//       component={ProfileScreen}
-//       options={{
-//         headerShown: false,
-//       }}
-//     />
-//     <Stack.Screen
-//       name="EditProfile"
-//       component={EditProfileScreen}
-//       options={{
-//         headerTitle: 'Edit Profile',
-//         headerBackTitleVisible: false,
-//         headerTitleAlign: 'center',
-//         headerStyle: {
-//           backgroundColor: '#fff',
-//           shadowColor: '#fff',
-//           elevation: 0,
-//         },
-//       }}
-//     />
-//   </Stack.Navigator>
-// );
+const MessageStack = ({ navigation }) => (
+  <Stack.Navigator
+    screenOptions={{
+      headerShown: false,
+    }}
+  >
+    <Stack.Screen name="GroupScreen" component={GroupScreen} />
+    <Stack.Screen
+      name="Chat"
+      component={ChatScreen}
+      options={({ route }) => ({
+        // title: route.params.userName,
+        headerBackTitleVisible: false,
+        tabBarVisible:false
+      })}
+    />
+  </Stack.Navigator>
+);
 
 const AppStack = () => {
   const getTabBarVisibility = (route) => {
@@ -135,6 +39,7 @@ const AppStack = () => {
       ? route.state.routes[route.state.index].name
       : "";
 
+      console.log('routeName: ', routeName);
     if (routeName === "Chat") {
       return false;
     }
@@ -149,14 +54,14 @@ const AppStack = () => {
       }}
     >
       <Tab.Screen
-        name="Group"
-        component={GroupScreen}
+        name="MessageStack"
+        component={MessageStack}
         options={({ route }) => ({
-          tabBarLabel: "Group",
-          // tabBarVisible: route.state && route.state.index === 0,
+          tabBarLabel: "Chat",
+          tabBarVisible: getTabBarVisibility(route),
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="home-outline"
+            <Ionicons
+              name="chatbox-ellipses-outline"
               color={color}
               size={size}
             />
@@ -164,8 +69,8 @@ const AppStack = () => {
         })}
       />
       <Tab.Screen
-        name="Chat"
-        component={ChatScreen}
+        name="Quotes"
+        component={QuotesScreen}
         options={({ route }) => ({
           tabBarVisible: getTabBarVisibility(route),
           // Or Hide tabbar when push!
@@ -185,7 +90,6 @@ const AppStack = () => {
         name="Profile"
         component={ProfileScreen}
         options={{
-          // tabBarLabel: 'Home',
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="person-outline" color={color} size={size} />
           ),
